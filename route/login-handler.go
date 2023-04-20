94c64f00faf99561250fd72f263f77ad3da0fea3
onedayonecommit <rudghks0981@gmail.com>
2023-04-20 18:09:19 +0900

    로그인 성공 혹은 실패 아직 Jwt 미구현

--- route/login-handler.go
+++ route/login-handler.go
@@ -1,12 +1,17 @@
 package route
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/onedayonecommit/sns/util"
+)
 
 type loginBody struct{
 	Email string `json:"email" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
 
+// Login handler
 func LoginHandler(res http.ResponseWriter,req *http.Request){
 	if req.Method != "POST" {
 		http.Error(res,"request method is wrong",http.StatusBadRequest)
@@ -14,4 +19,23 @@ func LoginHandler(res http.ResponseWriter,req *http.Request){
 	}
 
 	var l loginBody
+
+	err := reqBodyCheck(req,&l)
+
+	if err != nil {
+		http.Error(res,"request body is wrong",http.StatusBadRequest)
+		return
+	}
+
+	checkStatus,hashPw := UserCheck(l.Email)
+	if !checkStatus {
+		result := util.CompareHashPw(hashPw,l.Password)
+		if result {
+			// 성공시 Jwt 토큰 발급
+			res.WriteHeader(http.StatusOK)
+			return
+		}
+		http.Error(res,"Password do not match",http.StatusBadRequest)
+		return
+	}
 }
