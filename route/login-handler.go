69a4a9fc4c699c927dc8ef08210298fc3dd91601
onedayonecommit <rudghks0981@gmail.com>
2023-04-20 20:53:31 +0900

    제시된 content-type 확인 후 변경중

--- route/login-handler.go
+++ route/login-handler.go
@@ -12,24 +12,52 @@ type loginBody struct{
 }
 
 // Login handler
+// func LoginHandler(res http.ResponseWriter,req *http.Request){
+// 	if req.Method != "POST" {
+// 		http.Error(res,"request method is not allowed",http.StatusMethodNotAllowed)
+// 		return
+// 	}
+
+// 	var l loginBody
+
+// 	err := reqBodyCheck(req,&l)
+// 	if err != nil {
+// 		http.Error(res,"request body is wrong",http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	checkStatus,hashPw := UserCheck(l.Email)
+// 	if !checkStatus {
+// 		result := util.CompareHashPw(hashPw,l.Password)
+// 		if result {
+// 			// 성공시 Jwt 토큰 발급
+// 			res.WriteHeader(http.StatusOK)
+// 			return
+// 		}
+// 		http.Error(res,"Password do not match",http.StatusBadRequest)
+// 		return
+// 	}
+// }
 func LoginHandler(res http.ResponseWriter,req *http.Request){
 	if req.Method != "POST" {
 		http.Error(res,"request method is not allowed",http.StatusMethodNotAllowed)
 		return
 	}
 
-	var l loginBody
-
-	err := reqBodyCheck(req,&l)
-
-	if err != nil {
-		http.Error(res,"request body is wrong",http.StatusBadRequest)
+	// var l loginBody
+	err := req.ParseForm()
+	if err != nil{
+		http.Error(res,"content type is wrong",http.StatusBadRequest)
 		return
 	}
+	email := req.FormValue("email")
+	pw := req.FormValue("password")
+	// go fmt.Println(req.FormValue("email"))
+	// go fmt.Println(req.FormValue("password"))
 
-	checkStatus,hashPw := UserCheck(l.Email)
+	checkStatus,hashPw := UserCheck(email)
 	if !checkStatus {
-		result := util.CompareHashPw(hashPw,l.Password)
+		result := util.CompareHashPw(hashPw,pw)
 		if result {
 			// 성공시 Jwt 토큰 발급
 			res.WriteHeader(http.StatusOK)
