ae927ea4e4dcd1cf0c69757c60c23bacbb303576
onedayonecommit <rudghks0981@gmail.com>
2023-04-20 18:08:44 +0900

    change Password column length

--- mysql/model/user.go
+++ mysql/model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
     Id        uint   `gorm:"primaryKey;autoIncrement"` // 고유키 유저의 인덱스 번호
     Email     string `gorm:"type:varchar(50);not null;unique"` // 이메일
-    Password  string `gorm:"type:varchar(30);not null"` // 비밀번호
+    Password  string `gorm:"type:varchar(100);not null"` // 비밀번호
     FullName  string `gorm:"type:varchar(16);not null;column:full_name"` // 닉네임
     CreatedAt time.Time `gorm:"autoCreateTime;column:created_at"`
 }
