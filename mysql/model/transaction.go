eb1bf879eca45588c4751921dfc25009a631f692
onedayonecommit <rudghks0981@gmail.com>
2023-04-21 00:07:47 +0900

    필드이름 맨 앞 대문자 변경

--- mysql/model/transaction.go
+++ mysql/model/transaction.go
@@ -4,10 +4,10 @@ import "time"
 
 type Transaction struct{
 	ID uint `gorm:"autoIncrement;primaryKey;coulmn:transaction_id"`
-	TransactionId string `gorm:"varchar(32);unique;not null;"`
-	to string `gorm:"varchar(16);not null"`
-	from string `gorm:"varchar(16);not null"`
-	token string `gorm:"varchar(3);not null"`
-	amount uint `gorm:"type:decimal(23,18);not null"`
+	TransactionId string `gorm:"varchar(32);unique;not null"`
+	To string `gorm:"varchar(16);not null"`
+	From string `gorm:"varchar(16);not null"`
+	Token string `gorm:"varchar(3);not null"`
+	Amount uint `gorm:"type:decimal(23,18);not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime;column:created_at"`
 }
