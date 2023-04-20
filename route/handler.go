39b9c270ab0fef1fc4ee1b4d2964cad28a5b4de9
onedayonecommit <rudghks0981@gmail.com>
2023-04-21 02:14:51 +0900

    UserWallet return function

--- route/handler.go
+++ route/handler.go
@@ -1,24 +1,32 @@
 package route
 
 import (
-	"fmt"
-
 	"github.com/onedayonecommit/sns/mysql"
 	"github.com/onedayonecommit/sns/mysql/model"
 )
 
-// 가입된 데이터 없으면 True 있으면 false,password
-func UserCheck(userEmail string) (bool,string) {
-	fmt.Println(userEmail)
+// 가입된 데이터 없으면 err 있으면 nil,password
+func UserCheck(userEmail string) (error,string) {
 	db := mysql.ConnectDatabase()
 	var user model.User
 	err:= db.Where("email = ? ",userEmail).First(&user).Error
 	close,_ := db.DB()
 	defer close.Close() // 함수가 종료되면 연결하였던 DB 연결을 끊어줘야 메모리릭 방지 가능
 	if err != nil {
-		return true,"" // 전달받은 이메일로 가입된 데이터가 없을시
+		return err,"" // 전달받은 이메일로 가입된 데이터가 없을시
 	}
-	return false,user.Password // 전달 받은 이메일로 가입된 데이터가 있다면 패스워드 반환
+	return nil,user.Password // 전달 받은 이메일로 가입된 데이터가 있다면 패스워드 반환
 }
 
-// 전달받은 요청값 검증 실패시 err 성공시 nil
+func UserWallet(userEmail string) (string){
+	db := mysql.ConnectDatabase()
+	var wallet model.Wallet
+	close,_ := db.DB()
+	defer close.Close()
+
+	err:= db.Where("user_id = ? ",userEmail).First(&wallet).Error
+	if err !=nil {
+		return ""
+	}
+	return wallet.Wallet_address
+}
