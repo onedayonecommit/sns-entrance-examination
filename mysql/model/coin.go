ab08a6ec1321aa60bde995e16621bcb0e0ca42a6
onedayonecommit <rudghks0981@gmail.com>
2023-04-20 22:20:37 +0900

    ETH, BTC DataType Change

--- mysql/model/coin.go
+++ mysql/model/coin.go
@@ -1,8 +1,8 @@
 package model
 
 type Coin struct{
-	ETH uint `gorm:"type:decimal(23,18);not null;default:100"`
-	BTC uint `gorm:"type:decimal(23,18);not null;default:100"`
+	ETH float64 `gorm:"type:decimal(23,18);not null;default:100"`
+	BTC float64 `gorm:"type:decimal(23,18);not null;default:100"`
 	Address string `gorm:"type:varchar(16);not null"`
 	Wallet Wallet `gorm:"foreignKey:Address;references:Wallet_address"`
 }
