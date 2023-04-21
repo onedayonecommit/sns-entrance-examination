8a2b0ded65032f37acc5f1aa81d5a334af5233d6
onedayonecommit <rudghks0981@gmail.com>
2023-04-21 20:15:12 +0900

    varchar (16) => varchar(18)

--- mysql/model/transaction.go
+++ mysql/model/transaction.go
@@ -4,9 +4,9 @@ import "time"
 
 type Transaction struct{
 	ID uint `gorm:"autoIncrement;primaryKey;coulmn:transaction_id"`
-	To string `gorm:"varchar(18);not null"`
-	From string `gorm:"varchar(18);not null"`
-	Coin string `gorm:"varchar(3);not null"`
+	To string `gorm:"type:varchar(18);not null"`
+	From string `gorm:"type:varchar(18);not null"`
+	Coin string `gorm:"type:varchar(3);not null"`
 	Amount uint `gorm:"not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime;column:created_at"`
 }
--- mysql/model/wallet.go
+++ mysql/model/wallet.go
@@ -1,7 +1,7 @@
 package model
 
 type Wallet struct {
-    Wallet_address string `gorm:"type:varchar(16);not null;uniqueIndex"`
+    Wallet_address string `gorm:"type:varchar(18);not null;uniqueIndex"`
     UserId        string `gorm:"type:varchar(50);not null;primaryKey;foreignKey:Email"`
     User           User   `gorm:"references:Email"`
 }
