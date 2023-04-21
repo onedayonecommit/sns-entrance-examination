386c234090318d8c18256cc4501d3a7ba36c6794
onedayonecommit <rudghks0981@gmail.com>
2023-04-21 21:54:02 +0900

    all wallets balance handler fixing

--- route/wallet-handler.go
+++ route/wallet-handler.go
@@ -22,26 +22,31 @@ type Response struct{
 	Coins []CoinResult
 }
 
+type Wallets struct{
+	wallets []Response
+}
+
 func GetAllWalletHandler(res http.ResponseWriter, req *http.Request){
 	if req.Method != "GET" {
 		http.Error(res,"request method is not allowed",http.StatusMethodNotAllowed)
 		return
 	}
-	var w []struct{
-		Wallet_address string `json:"wallet_address"` // 한번에 여러개의 데이터를 받아야하므로 []
-	}
+	// var w []struct{
+	// 	Wallet_address []string `json:"wallets"` // 한번에 여러개의 데이터를 받아야하므로 []
+	// }
 	
 	db :=mysql.ConnectDatabase()
 	
-	err:= db.Table("wallets").Select("wallet_address").Find(&w).Error // wallets테이블에서 wallet_address 데이터만 모두 출력
-	fmt.Println(w)
+	var coin []model.Coin
+	// err:= db.Table("wallets").Select("wallet_address").Find(&w).Error // wallets테이블에서 wallet_address 데이터만 모두 출력
+	err:= db.Select("address,eth,btc").Find(&coin) // wallets테이블에서 wallet_address 데이터만 모두 출력
 	if err != nil {
 		res.WriteHeader(http.StatusOK)
 		fmt.Fprintln(res,"There is no list of verified wallets.") // 지갑목록이 없으면
 		return 
 	}
 	res.Header().Set("Content-Type","application/json")
-	json.NewEncoder(res).Encode(w)
+	json.NewEncoder(res).Encode("")
 
 	close,_ := db.DB()
 	defer close.Close()
